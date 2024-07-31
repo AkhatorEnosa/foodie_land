3efe239ef8a5a0256619b01581ba274423045fd5
AkhatorEnosa <osa@John-316.local>
2024-07-31 07:30:59 +0100

    Navbar, Hero + Slider components

---
+++ app/components/Hero.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Slider from './Slider'
+
+const Hero = () => {
+  return (
+    <section className='w-screen h-auto flex justify-center items-center gap-10 overflow-scroll'>
+        <Slider />
+    </section>
+  )
+}
+
+export default Hero
---
+++ app/components/Navbar.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { FaTwitter } from 'react-icons/fa'
+import { FaFacebookF, FaInstagram } from 'react-icons/fa6'
+
+const Navbar = () => {
+  return (
+    <section role='navbar' className='w-full flex justify-between items-center px-20 mb-20'>
+        <h1 className='logo text-2xl font-normal leading-[30px]'>Foodieland<span className='text-orange-500'>.</span></h1>
+        <ul className='flex justify-betweeen items-center gap-[60px] font-medium text-base leading-[19.38px] -tracking-[2%]'>
+            <li className='cursor-pointer hover:underline'>Home</li>
+            <li className='cursor-pointer hover:underline'>Recipe</li>
+            <li className='cursor-pointer hover:underline'>Blog</li>
+            <li className='cursor-pointer hover:underline'>Contact</li>
+            <li className='cursor-pointer hover:underline'>About Us</li>
+        </ul>
+        <div className='socials flex gap-10'>
+            <p className='cursor-pointer hover:text-black/50'><FaFacebookF /></p>
+            <p className='cursor-pointer hover:text-black/50'><FaTwitter /></p>
+            <p className='cursor-pointer hover:text-black/50'><FaInstagram /></p>
+        </div>
+    </section>
+  )
+}
+
+export default Navbar
