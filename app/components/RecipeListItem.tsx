15f5c896d6e8f83922d838410a18031f7a627c72
AkhatorEnosa <osa@John-316.local>
2024-08-01 15:16:47 +0100

    toggle check ingredient list

--- app/components/RecipeListItem.tsx
+++ app/components/RecipeListItem.tsx
@@ -11,9 +11,9 @@ const RecipeListItem = () => {
   }
   
   return (
-    <div className='flex gap-6 justify-start items-center hover:text-black/60 hover:cursor-pointer' onClick={()=>handleCheck()}>
+    <div className='flex gap-6 justify-start items-center hover:cursor-pointer' onClick={()=>handleCheck()}>
         {check  === false ? <RiCheckboxBlankCircleLine  className='text-2xl'/> : <RiCheckboxCircleFill  className='text-2xl'/>}
-        <p className={check  === false ? '' : 'line-through' }>Lorem ipsum dolor sit amet</p>
+        <p className={check  === false ? '' : 'line-through text-black/60' }>Lorem ipsum dolor sit amet</p>
     </div>
   )
 }
