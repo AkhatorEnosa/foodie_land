258c1902d728e91b96ff2057f26bc088f10bed98
AkhatorEnosa <osa@John-316.local>
2024-08-01 02:08:14 +0100

    Bug bounty

--- app/Hompage/page.tsx
+++ app/Hompage/page.tsx
@@ -1,26 +1,27 @@
-// import Image from "next/image";
+import Image from "next/image";
 // import Navbar from "./components/Home/Navbar";
-// import Hero from "./components/Home/Hero";
-// import Category from "./components/Home/Category";
-// import Recipes from "./components/Home/Recipes";
-// import LearnMore from "./components/Home/LearnMore";
-// import InstaPosts from "./components/Home/InstaPosts";
-// import MoreRecipe from "./components/Home/MoreRecipe";
-// import ContactUs from "./components/Home/ContactUs";
-// import Footer from "./components/Home/Footer";
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import Category from "./Category";
+import Recipes from "./Recipes";
+import LearnMore from ".//LearnMore";
+import InstaPosts from "./InstaPosts";
+import MoreRecipe from "./MoreRecipe";
+import ContactUs from "./ContactUs";
+import Footer from "./Footer";
 
-// export default function Home() {
-//   return (
-//     <main className="flex w-screen h-auto flex-col items-center py-10 leading-[16.94px] -tracking-[2%] ">
-//       <Navbar />
-//       <Hero />
-//       <Category />
-//       <Recipes />
-//       <LearnMore />
-//       <InstaPosts />
-//       <MoreRecipe />
-//       <ContactUs />
-//       <Footer />
-//     </main>
-//   );
-// }
+export default function Home() {
+  return (
+    <main className="flex w-screen h-auto flex-col items-center py-10 leading-[16.94px] -tracking-[2%] ">
+      <Navbar />
+      <Hero />
+      <Category />
+      <Recipes />
+      <LearnMore />
+      <InstaPosts />
+      <MoreRecipe />
+      <ContactUs />
+      <Footer />
+    </main>
+  );
+}
